f273f89c6e2fc82b95efdfb8b461194d13e08356
Bruno Lins Maia <bruno.maia@serpro.gov.br>
2024-12-23 14:33:55 -0300

    Validando objetos com ArgumentCaptor

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,16 +1,20 @@
 package br.com.alura.adopet.api.service;
 
 import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.BDDMockito;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
@@ -49,16 +53,27 @@ class AdocaoServiceTest {
     private Abrigo abrigo;
 
     private SolicitacaoAdocaoDto dto;
+    
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
 	
     @Test
 	void salvarAdocaoAoSolicitar() {
 		//ARRANGE
-		
+    	this.dto = new SolicitacaoAdocaoDto(10l,20l,"motivo qualquer");
+    	BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+    	BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+    	BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+    			
     	//ACT
         service.solicitar(dto);
         
       //ASSERT
-        BDDMockito.then(petRepository).should().save(ArgumentMatchers.any());
+        BDDMockito.then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+        Assertions.assertEquals(pet, adocaoSalva.getPet());
+        Assertions.assertEquals(tutor, adocaoSalva.getTutor());
+        Assertions.assertEquals(dto.motivo(), adocaoSalva.getMotivo());
 	}
 
 	
